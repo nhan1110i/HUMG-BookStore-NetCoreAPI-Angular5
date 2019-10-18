70608ca32bf0313fdd5c3c55f6b21e9265032465
Nhan1110i <trongnhan1110i@gmail.com>
2019-10-19 03:29:37 +0700

    decentralization complete

---
+++ BookStore/BookStore_Views/View/BookStore/src/app/Admin/services/employee/employee.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError,map,tap} from 'rxjs/operators';
+
+import {url,GetAuthorization} from '../../config/config';
+import { error } from '@angular/compiler/src/util';
+import { JsonPipe } from '@angular/common';
+import { Url } from 'url';
+const httpOptions = {
+  headers : new HttpHeaders({
+    'Content-Type' : 'application/json',
+    'Authorization' : GetAuthorization()
+  })
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  getEmployees(): Observable<any>{
+    return this.http.get<any>(url.host + url.employee.employees,httpOptions).pipe(
+      tap(rs =>{}),catchError(error => of ([]))
+    );
+  }
+  addEmployee(employee : any): Observable<any>{
+    return this.http.post<any>(url.host + url.employee.insert,employee,httpOptions).pipe(
+      tap(rs =>{}), catchError(error => of(console.log(error)))
+    )
+  }
+  updateEmployee(employee : any): Observable<any>{
+    return this.http.put(url.host + url.employee.update,employee,httpOptions).pipe(
+      tap(rs =>{}),catchError(error => of())
+    )
+  }
+  constructor(private http: HttpClient,) { }
+}
